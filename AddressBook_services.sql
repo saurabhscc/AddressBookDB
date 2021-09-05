27e569837e1b72a1d7b1573224f1720f14d8583e
saurabhs <saurabhpshirsath@gmail.com>
2021-09-05 08:47:53 +0530

    [saurabh]Add:No Of Contacts By Contact Type

--- AddressBook_services.sql
+++ AddressBook_services.sql
@@ -75,4 +75,10 @@ use addressBook_services
  /* Update the address book to add records in the recent added fields*/
  update addressBook_Table set addressBookType = 'Profession' where addressBookType is null
 
- 
+ /* UC10-Getting the contact count grouping by the contact type */
+ select * from addressBook_Table
+  /* Renaming the addressBookType columns to contactType in the address book table */
+ EXEC sp_rename 'addressBook_Table.addressBookType', 'contactType', 'COLUMN';
+ /* Getting the contact count grouping by the contact type */
+ select contactType, COUNT(*) as 'Number Of Contacts'
+from addressBook_Table group by contactType;
